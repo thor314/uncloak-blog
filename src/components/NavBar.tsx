1292335021ed9535bb72393978d42522bb89e172
Thor Kamphefner <thorck@pm.me>
2023-03-07 12:10:24 -0800

    nav search-bar html

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,14 +5,16 @@ export default function NavBar() {
   return (
     <>
       <nav className="top-bar">
-        <hr
+        {/* <hr
           role="separator"
           aria-orientation="horizontal"
           // class="v-divider theme--dark"
-        />
-        <Link href="/">Uncloak Blog</Link>
-        <a href="https://uncloak.org">Back to Wiki</a>
-        {/* todo search bar */}
+        /> */}
+        <Link href="/" className="nav-uncloak-link">Uncloak Blog</Link>
+        <a href="https://uncloak.org" className="nav-back-button">Back to Wiki</a>
+        <div className="nav-search-bar-container">
+          <input type="text" className="nav-search-bar-input" placeholder="Search" />
+        </div>
         {/* todo tags button */}
       </nav>
     </>
