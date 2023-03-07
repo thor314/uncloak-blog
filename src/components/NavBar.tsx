c3a21486484fa7df6068f5baf9c4fea021ea7d41
Rebecca Peckman <rebeccapeckman@gmail.com>
2023-03-07 13:44:42 -0800

    partial searchbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,20 @@
+import React from "react";
 import Link from "next/link";
 import Image from 'next/image';
-import React from "react";
+import SearchBar from './SearchBar';
 import styles from '@/styles/NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <nav className={styles.navBar}>
-      <Link href="/" className="nav-uncloak-link">
-        <Image src='https://wiki.uncloak.org/uncloak-head.jpg' width={34} height={34} alt='Uncloak Logo'/>
-        Uncloak Blog
-      </Link>
-      <a href="https://uncloak.org" className="nav-back-button">Back to Wiki</a>
-      <input type="text" className="nav-search-bar-input" placeholder="Search" />
-    </nav>
+    <header className={styles.NavBar}>
+      <nav className={styles.navLinks}>
+        <Link href="/" className={styles.brand}>
+          <Image src='https://wiki.uncloak.org/uncloak-head.jpg' width={34} height={34} alt='Uncloak Logo'/>
+          Uncloak Blog
+        </Link>
+        <a href="https://uncloak.org">Back to Wiki</a>
+      </nav>
+      <SearchBar/>
+    </header>
   );
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import styles from '@/styles/NavBar.module.css';
+
+export default function SearchBar() {
+    const [contents, setContents] = useState<string>('');
+
+    const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => setContents(evt.target.value);
+    // const clearContents = () => setContents('');
+
+    // TODO: add submit handler when we know what search behavior should be
+
+    return (
+        <div className={styles.SearchBar}>
+            <SearchIcon />
+            <input
+                className={styles.searchInput}
+                type="text"
+                placeholder="Search..."
+                value={contents}
+                onChange={handleChange}
+            />
+        </div>
+    );
+}
