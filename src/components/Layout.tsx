83a4e527f073d6271342a8a6e91225c0e267f181
Rebecca Peckman <rebeccapeckman@gmail.com>
2023-03-07 12:49:27 -0800

    added NavBar css module, Navbar logo and image next-config, added Roboto font to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { Roboto } from 'next/font/google';
 
 export type LayoutProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
+
+const roboto = Roboto({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 export default function Layout(props: LayoutProps) {
   return (
-    <>
+    <div className={roboto.className}>
       <NavBar />
       <main>{props.children}</main>
-    </>
+    </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,17 @@
 import Link from "next/link";
+import Image from 'next/image';
 import React from "react";
+import styles from '@/styles/NavBar.module.css';
 
 export default function NavBar() {
   return (
-    <>
-      <nav className="top-bar">
-        {/* <hr
-          role="separator"
-          aria-orientation="horizontal"
-          // class="v-divider theme--dark"
-        /> */}
-        <Link href="/" className="nav-uncloak-link">Uncloak Blog</Link>
-        <a href="https://uncloak.org" className="nav-back-button">Back to Wiki</a>
-        <input type="text" className="nav-search-bar-input" placeholder="Search" />
-        {/* todo tags button */}
-      </nav>
-    </>
+    <nav className={styles.navBar}>
+      <Link href="/" className="nav-uncloak-link">
+        <Image src='https://wiki.uncloak.org/uncloak-head.jpg' width={34} height={34} alt='Uncloak Logo'/>
+        Uncloak Blog
+      </Link>
+      <a href="https://uncloak.org" className="nav-back-button">Back to Wiki</a>
+      <input type="text" className="nav-search-bar-input" placeholder="Search" />
+    </nav>
   );
 }
