db980ee3cbe84e0a24ae45b1047e5b7beabd6cd0
Thor Kamphefner <thorck@pm.me>
2023-03-07 12:03:39 -0800

    formatting

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import NavBar from './NavBar';
+import React from "react";
+import NavBar from "./NavBar";
 
 export type LayoutProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 export default function Layout(props: LayoutProps) {
-    return (
-        <>
-            <NavBar/>
-            <main>{props.children}</main>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <main>{props.children}</main>
+    </>
+  );
 }
