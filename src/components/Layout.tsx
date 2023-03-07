a1a4683ff88a9fc24a81f44c9ca82c8fedec5c9b
Rebecca Peckman <rebeccapeckman@gmail.com>
2023-03-07 11:38:46 -0800

    empty layout component and custom app

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export type LayoutProps = {
+    children: React.ReactNode;
+}
+
+export default function Layout(props: LayoutProps) {
+    return (
+        <>
+        </>
+    );
+}
