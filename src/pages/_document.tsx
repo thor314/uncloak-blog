ba0708caa8fa51508eaa3b697e5abf3c7ba3fd7c
Thor Kamphefner <thorck@pm.me>
2023-03-09 18:03:05 -0800

    tmp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* tailwind: */}
+      {/* <body className="bg-white dark:bg-black text-gray-900 dark:text-white"> */}
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
